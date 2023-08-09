043f1d7e94f0690ebdfe7cd3dad412619015af29
Sehan Kwon <sehan2020@gmail.com>
2023-08-09 09:33:14 +0900

    logout error solved

--- lib/model/user_info_model.dart
+++ lib/model/user_info_model.dart
@@ -21,6 +21,11 @@ class UserInfoValueModel with ChangeNotifier {
     notifyListeners();
   }
 
+  void userEmailClear() {
+    userEmail = '';
+    notifyListeners();
+  }
+
   void valueUpdate() {
     isValueEntered = true;
     notifyListeners();
