843aa84d4381a9a5a16fb4698e4047ef0e540896
markFT39 <43339545+markFT39@users.noreply.github.com>
2023-08-14 21:46:34 +0900

    onBoarding, nickname update

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'package:nts/component/firefly.dart';
 import 'package:nts/component/navigationToggle.dart';
 import 'package:nts/component/nickname_sheet.dart';
 import 'package:nts/component/notification.dart';
+import 'package:nts/home/home_page_list_builder.dart';
 import 'package:nts/loading/loading_page.dart';
 import 'package:nts/login/login.dart';
 import 'package:nts/model/search_model.dart';
@@ -205,29 +206,12 @@ class BackgroundState extends State<Background> {
                         textColor: MyThemeColors.whiteColor,
                       );
                     }
-                    //  계정이 존재하고 닉네임이 있는 경우
-                    else if (snapshot.data == true) {
-                      WidgetsBinding.instance.addPostFrameCallback((_) {
-                        controller.fireFlyOn();
-                      });
-                      return HomePage(player: player);
-                    }
-                    //  계정이 존재하고 닉네임이 없는 경우
-                    else if (snapshot.data == false) {
-                      WidgetsBinding.instance.addPostFrameCallback(
-                        (_) {
-                          NickName().myNicknameSheet(
-                            context,
-                            Provider.of<UserInfoValueModel>(
-                              context,
-                              listen: false,
-                            ),
-                            0,
-                          );
-                        },
+                    //  로그인 성공 후
+                    else if (snapshot.hasData) {
+                      return HomePageListViewBuilder(
+                        player: player,
+                        firstPageIndex: snapshot.data!,
                       );
-
-                      return Container();
                     } else {
                       return Container();
                     }
@@ -267,9 +251,9 @@ class BackgroundState extends State<Background> {
   }
 }
 
-Future<bool> _getNickNameFromFirebase(UserInfoValueModel model) async {
+//  0: OnBoarding, 4: HomePage
+Future<int> _getNickNameFromFirebase(UserInfoValueModel model) async {
   if (model.userNickName.isEmpty) {
-    bool userNickNameIsMade = false;
     final userCollection = FirebaseFirestore.instance.collection("users");
     String? userId = FirebaseAuth.instance.currentUser?.uid;
 
@@ -279,12 +263,12 @@ Future<bool> _getNickNameFromFirebase(UserInfoValueModel model) async {
         Map<String, dynamic> userData =
             userSnapshot.data() as Map<String, dynamic>;
         //  check whether the nickName exist
-        if (userData.containsKey('nicknameMade')) {
-          userNickNameIsMade = userData['nicknameMade'];
-          model.userNickName = userData['nickname'];
-          model.userEmail = userData['email'];
+        if (userData.containsKey('nickname') && userData.containsKey('email')) {
+          model.userNickNameUpdate(userData['nickname']);
+          model.userEmailUpdate(userData['email']);
         } else {
           debugPrint('No field');
+          return 0;
         }
 
         QuerySnapshot querySnapshot = await FirebaseFirestore.instance
@@ -303,11 +287,11 @@ Future<bool> _getNickNameFromFirebase(UserInfoValueModel model) async {
     } else {
       debugPrint('User ID is null');
     }
-    //  delay for loading page
-    return Future.delayed(const Duration(seconds: 1), () {
-      return userNickNameIsMade;
-    });
+
+    if (model.userNickName.isEmpty) {
+      return 0;
+    }
   }
 
-  return true;
+  return 4;
 }
