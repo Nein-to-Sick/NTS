337f3aa730f9db579cacb6028923f55f4db91229
LocKey <rudfhr97@naver.com>
2023-08-17 13:37:04 +0900

    [feat] font size fixed

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) => MediaQuery(
+        data: MediaQuery.of(context).copyWith(
+          textScaleFactor: 1.0,
+        ),
+        child: child!,
+      ),
       theme: ThemeData(
         fontFamily: "SUITE",
         textSelectionTheme: TextSelectionThemeData(
